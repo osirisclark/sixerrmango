4ba093705491f878fa7b4f3704b44eacf55be65f
Osiris Gonzalez <OsirisMango@Osiriss-MacBook-Pro.local>
2017-02-22 20:05:56 -0500

    final commit before pushing to heroku

--- sixerr/settings.py
+++ sixerr/settings.py
@@ -137,7 +137,24 @@ LOGIN_REDIRECT_URL = '/'
 SOCIAL_AUTH_FACEBOOK_KEY = '708123669365596'
 SOCIAL_AUTH_FACEBOOK_SECRET = '0b22391a97a459607cad453a5231f7c3'
 
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'sixerrapp.social_auth_pipeline.save_avatar',# set the path to the function
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+)
+
 # Replace database setting to use postgresql on Heroku
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
+
+#Setup uploas directory for Gig model
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
---
+++ sixerrapp/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from .models import Gig
+
+
+class GigForm(ModelForm):
+    class Meta:
+        model = Gig
+        fields = ['title', 'category', 'description', 'price', 'photo', 'status']
